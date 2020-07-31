6c713c9a9e9309e0010c837cb4b68af39dfb73bf
Thuan Nguyen <huuthuan.nguyen@hotmail.com>
2020-07-31 16:44:24 +0700

    Update the thread model

--- chitchat/src/data/thread.go
+++ chitchat/src/data/thread.go
@@ -10,6 +10,21 @@ type Thread struct {
 	CreatedAt time.Time
 }
 
+type Post struct {
+	Id int
+	Uuid string
+	Body string
+	UserId int
+	ThreadId int
+	CreatedAt time.Time
+}
+
+// format the CreatedAt date to display nicely on the screen
+func (thread *Thread) CreatedAtDate() string {
+	return thread.CreatedAt.Format("Jan 2, 2006 at 3:04pm")
+}
+
+// get the number of posts in a thread
 func (thread *Thread) NumReplies() (count int) {
 	rows, err := Db.Query("SELECT count(*) FROM posts WHERE thread_id = $1", thread.Id)
 	if err != nil {
@@ -23,6 +38,7 @@ func (thread *Thread) NumReplies() (count int) {
 	_ = rows.Close()
 	return
 }
+
 
 func Threads() (threads []Thread, err error) {
 	rows, err := Db.Query("SELECT id, uuid, topic, user_id, created_at FROM threads ORDER BY created_at DESC")
