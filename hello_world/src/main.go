42929100199feee60f106ed9d12ecd3e86eba6c6
Thuan Nguyen <huuthuan.nguyen@hotmail.com>
2020-08-11 17:43:39 +0700

    Update the hello handler

--- hello_world/src/main.go
+++ hello_world/src/main.go
@@ -11,22 +11,34 @@ func (h *HelloHandler) ServeHTTP(writer http.ResponseWriter, request *http.Reque
 	fmt.Fprintf(writer, "Hello!")
 }
 
+func hello(writer http.ResponseWriter, request *http.Request) {
+	fmt.Fprintf(writer, "Hello!")
+}
+
 type WorldHandler struct{}
 
 func (h *WorldHandler) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
 	fmt.Fprintf(writer, "World!")
 }
 
+func world(writer http.ResponseWriter, request *http.Request) {
+	fmt.Fprintf(writer, "World!")
+}
+
 func main() {
-	hello := HelloHandler{}
-	world := WorldHandler{}
+	// hello := HelloHandler{}
+	// world := WorldHandler{}
+	helloHandler := http.HandlerFunc(hello)
 
 	server := http.Server{
 		Addr: "127.0.0.1:8080",
 	}
 
-	http.Handle("/hello", &hello)
-	http.Handle("/world", &world)
+	// http.Handle("/hello", &hello)
+	// http.Handle("/world", &world)
+	// http.HandleFunc("/hello", hello)
+	http.Handle("/hello", &helloHandler)
+	http.HandleFunc("/world", world)
 
 	server.ListenAndServe()
 }
